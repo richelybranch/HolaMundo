ccbc17181a1dd64b026e861710e0c3b0e6b5282b
RyCry7 <richely.almeida825@ist17dejulio.edu.ec>
2023-11-13 18:06:04 -0500

    comentario

--- app/src/main/java/com/rycry/holamundo/Formulario.java
+++ app/src/main/java/com/rycry/holamundo/Formulario.java
@@ -3,6 +3,10 @@ package com.rycry.holamundo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Formulario extends AppCompatActivity {
 
@@ -10,5 +14,25 @@ public class Formulario extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario);
+
+
+        Button btnIngresar= findViewById(R.id.btnIngresar);
+        EditText edNombre, edApellido, edCedula, edCorreo, edDireccion;
+        edNombre=findViewById(R.id.txtNombre);
+        edApellido = findViewById(R.id.txtApellido);
+        edCedula = findViewById(R.id.txtCedula);
+        edCorreo = findViewById(R.id.txtCorreo);
+        edDireccion = findViewById(R.id.txtDireccion);
+
+        btnIngresar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String nombre=edNombre.getText().toString();
+                if(!nombre.isEmpty()){
+                    Toast.makeText(Formulario.this, "Informacion ingresada fokin correctamente", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
     }
 }
